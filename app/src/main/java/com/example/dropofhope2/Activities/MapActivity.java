5c19c609e0ed5cc167c6b66e012a3d3418b10643
user_13 <28-user_13@users.noreply.ccgit.in>
2020-05-21 02:13:10 +0530

    new_map myLocationUpdates added

--- app/src/main/java/com/example/dropofhope2/Activities/MapActivity.java
+++ app/src/main/java/com/example/dropofhope2/Activities/MapActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
 import android.Manifest;
 import android.app.Dialog;
@@ -14,8 +16,11 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.util.TypedValue;
@@ -27,6 +32,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -34,10 +40,17 @@ import android.widget.Toast;
 import com.example.dropofhope2.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -59,8 +72,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private GoogleMap mGoogleMap;
     private SupportMapFragment supportMapFragment;
+    private FusedLocationProviderClient mLocationClient;
+    private LocationCallback mLocationCallback;
+    private HandlerThread mHandlerThread;
 
     private EditText searchEt;
+    private ImageView getLocationIv;
     private String userAddress;
 
     @Override
@@ -69,18 +86,37 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_map);
         initGoogleMap();
         searchEt = findViewById(R.id.search_location);
+        getLocationIv = findViewById(R.id.get_location);
         userAddress = Objects.requireNonNull(getIntent().getExtras()).getString("Address");
+
+        mLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                Location location = locationResult.getLastLocation();
+                runOnUiThread(() -> {
+                    gotoLocation(location.getLatitude(), location.getLongitude());
+                    Log.d(TAG, "runOnUiThread : Thread name : " + Thread.currentThread().getName());
+                });
+                //showMarker(location.getLatitude(), location.getLongitude());
+                Log.d(TAG, location.getLatitude() + ", " + location.getLongitude());
+                Log.d(TAG, "Thread name : " + Thread.currentThread().getName());
+            }
+        };
+        getLocationIv.setOnClickListener(v -> getLocationUpdates());
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d(TAG, "Map is showing");
         mGoogleMap = googleMap;
-        mGoogleMap.setMyLocationEnabled(true);
-        getUserLocation(userAddress);
-        init();
+        //mGoogleMap.setMyLocationEnabled(true);
         mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
         mGoogleMap.getUiSettings().setMapToolbarEnabled(true);
+        getUserLocation(userAddress);
         try {
             final ViewGroup parent = (ViewGroup) Objects.requireNonNull(supportMapFragment.getView()).findViewWithTag("GoogleMapMyLocationButton").getParent();
             parent.post(() -> {
@@ -102,6 +138,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        init();
     }
 
     private void init() {
@@ -146,6 +183,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM));
             MarkerOptions markerOptions = new MarkerOptions()
                     .title(userAddress)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
                     .position(new LatLng(address.getLatitude(), address.getLongitude()));
             mGoogleMap.addMarker(markerOptions);
         }
@@ -318,7 +356,58 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     }
 
+    private void getCurrentLocation() {
+        mLocationClient.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Location location = task.getResult();
+                gotoLocation(location.getLatitude(), location.getLongitude());
+            } else {
+                Log.d(TAG, "getCurrentLocation: Error: " + Objects.requireNonNull(task.getException()).getMessage());
+            }
+        });
+    }
+
+    private void gotoLocation(double lat, double lng) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM);
+        mGoogleMap.animateCamera(cameraUpdate);
+    }
+
+    private void getLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setInterval(10 * 1000);
+        locationRequest.setFastestInterval(5000);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return ;
+        }
+        mHandlerThread = new HandlerThread("LocationCallbackThread");
+        mHandlerThread.start();
+        mLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, mHandlerThread.getLooper());
+    }
+
+    private void showMarker(double lat, double lng) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(new LatLng(lat, lng));
+        mGoogleMap.addMarker(markerOptions);
+    }
+
     private void hideSoftKeyboard() {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mLocationCallback != null) {
+            mLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHandlerThread != null) {
+            mHandlerThread.quit();
+        }
     }
 }
